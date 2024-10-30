e527f753e9ff7edae49921ef4d9c535557370126
NoisPv <leandro24pv@gmail.com>
2024-10-30 00:42:09 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_RE5C9F-user
-add_lunch_combo omni_RE5C9F-userdebug
-add_lunch_combo omni_RE5C9F-eng
+add_lunch_combo twrp_RE5C9F-user
+add_lunch_combo twrp_RE5C9F-userdebug
+add_lunch_combo twrp_RE5C9F-eng

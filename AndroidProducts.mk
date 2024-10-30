3d0e76de742ef661fcc81482e6403c160c49239c
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-30 03:57:56 +0100

    RE5C9F: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: ums9230_hulk_Natv Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RE5C9F.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RE5C9F-user \
+    omni_RE5C9F-userdebug \
+    omni_RE5C9F-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for realme ums9230_hulk_Natv (RE5C9F)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```

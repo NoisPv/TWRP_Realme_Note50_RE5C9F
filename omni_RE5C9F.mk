352c58f69649aca7358003f725c581ac1f59f56b
NoisPv <leandro24pv@gmail.com>
2024-11-03 12:05:07 -0300

    Atualizar o twrp_RE5C9F.mk

--- omni_RE5C9F.mk
+++ omni_RE5C9F.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RE5C9F device
 $(call inherit-product, device/realme/RE5C9F/device.mk)
 
 PRODUCT_DEVICE := RE5C9F
-PRODUCT_NAME := omni_RE5C9F
+PRODUCT_NAME := twrp_RE5C9F
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := ums9230_hulk_Natv
 PRODUCT_MANUFACTURER := realme
